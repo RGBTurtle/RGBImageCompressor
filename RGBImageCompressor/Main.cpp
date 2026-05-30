be938176dacfe77d8dbe1cda2dec8d251b12d621
RGBTurtle <leohelton@outlook.com>
2026-05-30 22:38:02 +1200

    Yeah well it works ;)

--- RGBImageCompressor/Main.cpp
+++ RGBImageCompressor/Main.cpp
@@ -4,14 +4,16 @@
 #include <stdint.h>
 #include <array>
 #include <vector>
+#include <cmath>
+#include <numbers>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "include/stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "include/stb/stb_image_write.h"
 
-const int width = 50;
-const int height = 50;
+const int width = 300;
+const int height = 300;
 const int greyChannels = 3;
 
 unsigned char *grey_img;
@@ -23,7 +25,8 @@ std::array<unsigned char, 3> color_by_distance(std::array<unsigned char, 3> colo
     if (distance > 1){
         distance = 1;
     }
-        std::array<unsigned char, 3> newColor;
+    distance = 1 - distance;
+    std::array<unsigned char, 3> newColor;
     newColor[0] = (unsigned char)((int)color[0] * distance);
     newColor[1] = (unsigned char)((int)color[1] * distance);
     newColor[2] = (unsigned char)((int)color[2] * distance);
@@ -31,15 +34,18 @@ std::array<unsigned char, 3> color_by_distance(std::array<unsigned char, 3> colo
 }
 
 float distance_to_center(unsigned char* pixel, unsigned char* start, std::array<int16_t, 2> scale){
-    int16_t pixelpos = (pixel - grey_img) / greyChannels;
-    int16_t startpos = (start - grey_img) / greyChannels;
-    int16_t relapos = pixelpos - startpos;
+    int32_t pixelpos = (pixel - grey_img) / greyChannels;
+    int32_t startpos = (start - grey_img) / greyChannels;
+    int32_t relapos = pixelpos - startpos;
     std::array<float, 2> vector = {pixelpos % width, pixelpos / width};
     vector[0] -= startpos % width;
     vector[1] -= startpos / width;
-    vector[0] /= scale[0];
-    vector[1] /= scale[1];
-    return float(sqrt((vector[0] * vector[0]) + (vector[1] * vector[1])));
+    vector[0] -= scale[0] / 2;
+    vector[1] -= scale[1] / 2;
+
+    vector[0] /= scale[0] / 2;
+    vector[1] /= scale[1] / 2;
+    return float(sqrt((vector[0] * vector[0]) + (vector[1] * vector[1])));//std::pow(2.71828182845904523536, std::pow(-(sqrt((vector[0] * vector[0]) + (vector[1] * vector[1]))), 2)));
 }
 
 
@@ -85,7 +91,10 @@ std::vector<splat> splats;
 
 
 int main(){
-    splats.push_back(splat( {5, 5}, {40, 40}, {255, 0, 255} ));
+    splats.push_back(splat( {5, 5}, {70, 140}, {255, 0, 0} ));
+    splats.push_back(splat( {70, 5}, {60, 140}, {0, 255, 255} ));
+    splats.push_back(splat( {5, 50}, {400, 80}, {0, 0, 255} ));
+    splats.push_back(splat( {5, 5}, {49, 49}, {255, 255, 255} ));
 
     grey_img_size = width * height * greyChannels;
 
@@ -104,9 +113,6 @@ int main(){
     for(int i = 0 ;i != splats.size(); i++){
         splats[i].paint();
     }
-
-    //std::ofstream outFile("GreenDom.RGBIC", std::ios_base::trunc);
-
 
     stbi_write_jpg("output.jpg", width, height, greyChannels, grey_img, 100);
     return 1;
